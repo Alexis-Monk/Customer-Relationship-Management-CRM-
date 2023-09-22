a0254c3b8c65de020ea5bb2145cf47a47b23e5a9
Alexis-Monk <126738117+Alexis-Monk@users.noreply.github.com>
2023-09-21 22:13:49 -0600

    Finish for first submit

--- main.go
+++ main.go
@@ -8,114 +8,195 @@ import (
 	"net/http"
 )
 
-var clients = map[string]string{
-	"1": "Alex",
-	"2": "Lakeisha",
-	"3": "Itzel",
-	"4": "Pablo",
-	"5": "Ximena",
-	"6": "Arturo",
-	"7": "Paola",
+type Customer struct {
+	ID        uint   `json:"id"`
+	Name      string `json:"name"`
+	Role      string `json:"role"`
+	Email     string `json:"email"`
+	Phone     uint   `json:"phone"`
+	Contacted bool   `json:"contacted"`
 }
 
-func getClients(w http.ResponseWriter, r *http.Request) {
+var customers = map[string]Customer{
+	"1": Customer{
+		ID:        1,
+		Name:      "Alex",
+		Role:      "Customer",
+		Email:     "alex@gmail.com",
+		Phone:     5552223344,
+		Contacted: true,
+	},
+	"2": Customer{
+		ID:        2,
+		Name:      "Lake",
+		Role:      "Customer",
+		Email:     "lake@gmail.com",
+		Phone:     4445557788,
+		Contacted: true,
+	},
+	"3": Customer{
+		ID:        3,
+		Name:      "Itzel",
+		Role:      "Customer",
+		Email:     "itzel@gmail.com",
+		Phone:     4449995533,
+		Contacted: false,
+	},
+}
+
+func getCustomers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	json.NewEncoder(w).Encode(clients)
+	json.NewEncoder(w).Encode(customers)
 }
 
-func getClientId(w http.ResponseWriter, r *http.Request) {
+func getCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	client, found := clients[id]
+	customer, found := customers[id]
 
 	if !found {
 		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(map[string]string{"error": "Client not found"})
+		json.NewEncoder(w).Encode(map[string]string{"error": "Customer not found"})
 		return
-	} else {
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(map[string]string{id: client})
 	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(customer)
 }
 
-func createClient(w http.ResponseWriter, r *http.Request) {
+func addCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	var newClient map[string]string
+	var newCustomer Customer
 
 	reqBody, _ := ioutil.ReadAll(r.Body)
 
-	json.Unmarshal(reqBody, &newClient)
-
-	for k, v := range newClient {
-		if _, ok := clients[k]; ok {
-			w.WriteHeader(http.StatusConflict)
-		} else {
-			clients[k] = v
-			w.WriteHeader(http.StatusCreated)
-		}
-
-		json.NewEncoder(w).Encode(clients)
-	}
-}
-
-func updateClient(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	_, found := clients[id]
-
-	if !found {
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(map[string]string{"error": "Client not found"})
-		return
-	}
-	var updateClient map[string]string
-
-	if err := json.NewDecoder(r.Body).Decode(&updateClient); err != nil {
+	err := json.Unmarshal(reqBody, &newCustomer)
+	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid JSON"})
 		return
 	}
 
-	updateValue, found := updateClient[id]
-	if found {
-		clients[id] = updateValue
+	id := fmt.Sprintf("%d", newCustomer.ID)
+
+	_, ok := customers[id]
+
+	if ok {
+		w.WriteHeader(http.StatusConflict)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Customer already exists"})
+		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"message": "Definition update successfully"})
+	customers[id] = newCustomer
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newCustomer)
 }
 
-func deleteClient(w http.ResponseWriter, r *http.Request) {
+func updateCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	customer, found := customers[id]
+
+	if !found {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Customer not found"})
+		return
+	}
+
+	var updateData map[string]interface{}
+
+	if err := json.NewDecoder(r.Body).Decode(&updateData); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid JSON"})
+		return
+	}
+
+	for key, value := range updateData {
+		switch key {
+		case "name":
+			customer.Name = value.(string)
+		case "role":
+			customer.Role = value.(string)
+		case "email":
+			customer.Email = value.(string)
+		case "phone":
+			customer.Phone = uint(value.(float64))
+		case "contacted":
+			customer.Contacted = value.(bool)
+		}
+	}
+
+	customers[id] = customer
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(customer)
+}
+
+func deleteCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	id := mux.Vars(r)["id"]
 
-	if _, ok := clients[id]; ok {
-		delete(clients, id)
+	if _, ok := customers[id]; ok {
+		delete(customers, id)
 		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(clients)
+		json.NewEncoder(w).Encode(customers)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(clients)
+		json.NewEncoder(w).Encode(customers)
 	}
+}
+
+func getAPIInfo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	apiInfo := map[string]interface{}{
+		"api_name":    "Customers API",
+		"description": "This is an API for managing customers.",
+		"endpoints": []map[string]string{
+			{
+				"url":         "/customers",
+				"description": "Get a list of customers.",
+			},
+			{
+				"url":         "/customers/{id}",
+				"description": "Get details of a customer.",
+			},
+			{
+				"url":         "/customers",
+				"description": "Add a new customer.",
+			},
+			{
+				"url":         "/customers/{id}",
+				"description": "Update an existing customer.",
+			},
+			{
+				"url":         "/customers/{id}",
+				"description": "Delete an existing customer.",
+			},
+		},
+	}
+	json.NewEncoder(w).Encode(apiInfo)
 }
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/clients", getClients).Methods("GET")
-	router.HandleFunc("/client/{id}", getClientId).Methods("GET")
-	router.HandleFunc("/postClient", createClient).Methods("POST")
-	router.HandleFunc("/updateClient/{id}", updateClient).Methods("PATCH")
-	router.HandleFunc("/deleteClient/{id}", deleteClient).Methods("DELETE")
+	router.HandleFunc("/customers", getCustomers).Methods("GET")
+	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
+	router.HandleFunc("/customers", addCustomer).Methods("POST")
+	router.HandleFunc("/customers/{id}", updateCustomer).Methods("PATCH")
+	router.HandleFunc("/customers/{id}", deleteCustomer).Methods("DELETE")
+	router.HandleFunc("/", getAPIInfo).Methods("GET")
 
 	fmt.Println("The server is running on port 3000...")
 	http.ListenAndServe(":3000", router)
